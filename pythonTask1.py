d9b964f9cf61f9f568b6ac0924bd270e43c28809
NWANKWO HENRY IFUNANYA <19147572+nwankwohenry@users.noreply.github.com>
2020-03-25 12:54:25 +0100

    Add files via upload

---
+++ pythonTask1.py
@@ -0,0 +1,8 @@
+"""
+A Python program that accepts the radius
+of a circle from the user and computes the area.
+"""
+
+radiusOfCircle = float(input("Enter the radius of the circle: "))
+areaOfCircle = (22/7)*(radiusOfCircle * radiusOfCircle)
+print("Area of the circle = " + str(areaOfCircle))
